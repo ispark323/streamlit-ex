0f8d9f81250dfeeaa2b6dced71e945f8e28b1fac
Sungmin Park <spark323@gmail.com>
2022-02-23 07:17:55 +0900

    file upload

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 import imghdr
 import streamlit as st
 
+
+st.title("File Upload")
+
+file_uploaded = st.file_uploader("Choose a file")
+
+if file_uploaded is not None:
+    st.image(file_uploaded, use_column_width=True)
+
+
 st.title("Upload Picture")
 
 img = st.camera_input("Upload an image")
