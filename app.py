744f00648a0a35379da0f845b71184b6ded8f384
Sungmin Park <spark323@gmail.com>
2022-03-05 21:02:34 +0900

    Youtube video

--- app.py
+++ app.py
@@ -1,18 +1,13 @@
-import imghdr
 import streamlit as st
+import streamlit.components.v1 as components
 
+st.subheader("【伝説】幻の十文字大根、収穫してみた！")
+st.video('https://youtu.be/aaFhoLlEkIQ') 
 
-st.title("File Upload")
+components.html(
+    """
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/aaFhoLlEkIQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    """,
+    height=315,
+)
 
-file_uploaded = st.file_uploader("Choose a file")
-
-if file_uploaded is not None:
-    st.image(file_uploaded, use_column_width=True)
-
-
-st.title("Upload Picture")
-
-img = st.camera_input("Upload an image")
-
-if img:
-    st.image(img)
